98aa1b7497b0e53370017f5f86d60870eaf0f4a3
muhammadrizo <mt936425155@gmail.com>
2024-11-07 00:05:19 +0500

    two tasks

---
+++ main.sql
@@ -0,0 +1,33 @@
+-- 1-chi topshiriq
+
+-- DROP TABLE IF EXISTS student;
+-- CREATE TABLE IF NOT EXISTS student(
+--     student_id SERIAL UNIQUE,
+--     name1 TEXT NOT NULL,
+--     age INTEGER,
+--     address TEXT
+-- );
+
+-- INSERT INTO student(name1, age, address) VALUES
+-- ('Muhammadrizo', 14, 'A.Navoiy'),
+-- ('Safobek', 16, 'Oltariq'),
+-- ('Alijanov', 17, 'Oltariq');
+
+-- SELECT * FROM student;
+-- 2-chi topshiriq
+-- DROP TABLE IF EXISTS products;
+
+
+-- CREATE TABLE IF NOT EXISTS products(
+-- 	pro_id SERIAL UNIQUE,
+-- 	pro_name TEXT NOT NULL,
+-- 	prise INTEGER,
+-- 	description TEXT
+-- );
+
+-- INSERT INTO products(pro_name, prise, description) VALUES
+-- ('Phone', 1000, 'Bla-Bla'),
+-- ('Laptop', 2000, 'So Good!'),
+-- ('House', 10000000, 'Very Big House');
+
+-- SELECT * FROM products;
